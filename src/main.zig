777f6611bded15cb269addddc95e456e51a27e1c
Hayes Barber <barberhayes@gmail.com>
2026-05-16 17:45:50 -0400

    game start

--- src/main.zig
+++ src/main.zig
@@ -40,8 +40,8 @@ pub fn main(init: std.process.Init) !void {
     try terminal.printANSICode(stdout_writer, terminal.ANSICode.hide_cursor);
     try terminal.printANSICode(stdout_writer, terminal.ANSICode.enter_alternate_buffer);
     defer {
-        terminal.printANSICode(stdout_writer, terminal.ANSICode.show_cursor) catch {};
         terminal.printANSICode(stdout_writer, terminal.ANSICode.exit_alternate_buffer) catch {};
+        terminal.printANSICode(stdout_writer, terminal.ANSICode.show_cursor) catch {};
     }
 
     const allocator = init.gpa;
@@ -54,6 +54,7 @@ pub fn main(init: std.process.Init) !void {
 
     try curr_buff.loadString(start_screen);
     try render.renderBuff(&prev_buff, &curr_buff, stdout_writer);
+    @memcpy(prev_buff.data, curr_buff.data);
 
     var game_state = game.GameState.init(size.rows, size.cols);
 
@@ -66,13 +67,20 @@ pub fn main(init: std.process.Init) !void {
             .f => {},
             .space => {
                 switch (game_state.mode) {
-                    .start_screen => game_state.mode = .playing,
+                    .start_screen => {
+                        game_state.mode = .playing;
+                        curr_buff.clear();
+                    },
                     else => {},
                 }
             },
             .esc => break :game_loop,
             else => {},
         }
+
+        try render.renderBuff(&prev_buff, &curr_buff, stdout_writer);
+
+        std.mem.swap(render.ScreenBuff, &prev_buff, &curr_buff);
 
         io.sleep(std.Io.Duration.fromNanoseconds(dt_ns), .awake) catch {};
     }
