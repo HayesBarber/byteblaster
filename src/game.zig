2a1cf13d548962f57d9dbbeaea71a36765b2ad9d
Hayes Barber <barberhayes@gmail.com>
2026-05-16 18:54:53 -0400

    spawn lazers

--- src/game.zig
+++ src/game.zig
@@ -1,11 +1,27 @@
 const render = @import("render.zig");
 const terminal = @import("terminal.zig");
 
+const MAX_LAZERS = 64;
+
+pub const Lazer = struct {
+    row: usize,
+    col: usize,
+
+    pub fn init(row: usize, col: usize) Lazer {
+        return .{
+            .row = row,
+            .col = col,
+        };
+    }
+};
+
 pub const GameState = struct {
     player_col: usize,
     cols: usize,
     rows: usize,
     mode: Mode,
+    lazers: [MAX_LAZERS]Lazer,
+    lazer_count: usize,
 
     pub fn init(rows: usize, cols: usize) GameState {
         return .{
@@ -13,6 +29,8 @@ pub const GameState = struct {
             .cols = cols,
             .rows = rows,
             .mode = .start_screen,
+            .lazers = undefined,
+            .lazer_count = 0,
         };
     }
 
@@ -28,6 +46,28 @@ pub const GameState = struct {
         }
     }
 
+    fn spawnLaser(self: *GameState) void {
+        if (self.lazer_count >= MAX_LAZERS) return;
+
+        self.lazers[self.lazer_count] = Lazer.init(self.rows - 2, self.player_col);
+        self.lazer_count += 1;
+    }
+
+    pub fn updateLazers(self: *GameState) void {
+        var i: usize = 0;
+
+        while (i < self.lazer_count) : (i += 1) {
+            if (self.lazers[i].row == 0) {
+                // remove by swapping with last
+                self.lazers[i] = self.lazers[self.lazer_count - 1];
+                self.lazer_count -= 1;
+                continue;
+            }
+
+            self.lazers[i].row -= 1;
+        }
+    }
+
     pub fn tick(self: *GameState, buff: *render.ScreenBuff, input: terminal.GameInput) void {
         if (self.mode != Mode.playing and input != .space) return;
 
@@ -36,11 +76,19 @@ pub const GameState = struct {
         switch (input) {
             .j => self.moveLeft(),
             .k => self.moveRight(),
+            .f => self.spawnLaser(),
             .space => self.mode = .playing,
             else => {},
         }
 
+        // update player
         buff.set(buff.rows - 1, self.player_col, glyph(.player));
+
+        //update lazers
+        self.updateLazers();
+        for (self.lazers[0..self.lazer_count]) |l| {
+            buff.set(l.row, l.col, glyph(.laser));
+        }
     }
 };
 
