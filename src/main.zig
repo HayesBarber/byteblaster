e44ca7ea43b8f7c4d3d89b4c936c511c18b94a9c
William Hayes Barber <barberhayes@gmail.com>
2026-05-16 11:53:16 -0400

    Init game loop and handle terminal input (#1)

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,42 @@
 const std = @import("std");
 const Io = std.Io;
 
+const terminal = @import("terminal.zig");
+
+const dt_ns = 16_666_667;
+
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
 
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
-    try stdout_writer.print("byteblaster\n", .{});
-    try stdout_writer.flush();
+
+    const original = try terminal.enableRawMode();
+    defer terminal.disableRawMode(original) catch {};
+
+    try terminal.printANSICode(stdout_writer, terminal.ANSICode.enter_alternate_buffer);
+    defer terminal.printANSICode(stdout_writer, terminal.ANSICode.exit_alternate_buffer) catch {};
+
+    game_loop: while (true) {
+        const input = terminal.pollInput() catch break :game_loop;
+
+        switch (input) {
+            .j => {
+                try stdout_writer.print("j", .{});
+                try stdout_writer.flush();
+            },
+            .k => {
+                try stdout_writer.print("k", .{});
+                try stdout_writer.flush();
+            },
+            .esc => break :game_loop,
+            else => {},
+        }
+
+        // update();
+        // render();
+
+        io.sleep(std.Io.Duration.fromNanoseconds(dt_ns), .awake) catch {};
+    }
 }
---
+++ src/terminal.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+const Io = std.Io;
+const posix = std.posix;
+
+pub const ANSICode = enum {
+    enter_alternate_buffer,
+    exit_alternate_buffer,
+    clear_screen,
+    hide_cursor,
+};
+
+pub const GameInput = enum(i16) {
+    noop = -1,
+    j = 'j',
+    k = 'k',
+    esc = 27,
+    _,
+};
+
+fn codeToRaw(code: ANSICode) []const u8 {
+    return switch (code) {
+        .enter_alternate_buffer => "\x1b[?1049h",
+        .exit_alternate_buffer => "\x1b[?1049l",
+        .clear_screen => "\x1b[2J",
+        .hide_cursor => "\x1b[?25l",
+    };
+}
+
+fn parseInput(byte: u8) GameInput {
+    return @enumFromInt(byte);
+}
+
+pub fn printANSICode(writer: *Io.Writer, comptime code: ANSICode) !void {
+    try writer.print(codeToRaw(code), .{});
+    try writer.flush();
+}
+
+pub fn enableRawMode() !posix.termios {
+    const stdin_fd = posix.STDIN_FILENO;
+
+    const original = try posix.tcgetattr(stdin_fd);
+    var raw = original;
+
+    raw.lflag.ECHO = false;
+    raw.lflag.ICANON = false;
+
+    try posix.tcsetattr(
+        stdin_fd,
+        .FLUSH,
+        raw,
+    );
+
+    return original;
+}
+
+pub fn disableRawMode(original: posix.termios) !void {
+    try posix.tcsetattr(
+        posix.STDIN_FILENO,
+        .FLUSH,
+        original,
+    );
+}
+
+pub fn pollInput() !GameInput {
+    var fds = [_]posix.pollfd{
+        .{
+            .fd = posix.STDIN_FILENO,
+            .events = posix.POLL.IN,
+            .revents = 0,
+        },
+    };
+
+    // 0 timeout = non-blocking poll
+    const n = try posix.poll(&fds, 0);
+
+    // no input this frame
+    if (n == 0) return GameInput.noop;
+
+    // stdin not readable
+    if ((fds[0].revents & posix.POLL.IN) == 0) return GameInput.noop;
+
+    var buf: [1]u8 = undefined;
+    _ = try posix.read(posix.STDIN_FILENO, &buf);
+
+    return parseInput(buf[0]);
+}
