ef35a54e4f73093bbcab4e354287086b1b606205
Hayes Barber <barberhayes@gmail.com>
2026-05-16 10:11:39 -0400

    typo

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ pub fn main(init: std.process.Init) !void {
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
 
-    try terminal.printANSIICode(stdout_writer, terminal.ANSII_Code.enter_alternate_buffer);
+    try terminal.printANSICode(stdout_writer, terminal.ANSI_Code.enter_alternate_buffer);
     io.sleep(std.Io.Duration.fromSeconds(1), .awake) catch return;
-    try terminal.printANSIICode(stdout_writer, terminal.ANSII_Code.exit_alternate_buffer);
+    try terminal.printANSICode(stdout_writer, terminal.ANSI_Code.exit_alternate_buffer);
 }
--- src/terminal.zig
+++ src/terminal.zig
@@ -1,23 +1,23 @@
 const std = @import("std");
 const Io = std.Io;
 
-pub const ANSII_Code = enum {
+pub const ANSI_Code = enum {
     enter_alternate_buffer,
     exit_alternate_buffer,
     clear_screen,
     hide_cursor,
 };
 
-fn codeToRaw(code: ANSII_Code) []const u8 {
+fn codeToRaw(code: ANSI_Code) []const u8 {
     return switch (code) {
-        ANSII_Code.enter_alternate_buffer => "\x1b[?1049h",
-        ANSII_Code.exit_alternate_buffer => "\x1b[?1049l",
-        ANSII_Code.clear_screen => "\x1b[2J",
-        ANSII_Code.hide_cursor => "\x1b[?25l",
+        ANSI_Code.enter_alternate_buffer => "\x1b[?1049h",
+        ANSI_Code.exit_alternate_buffer => "\x1b[?1049l",
+        ANSI_Code.clear_screen => "\x1b[2J",
+        ANSI_Code.hide_cursor => "\x1b[?25l",
     };
 }
 
-pub fn printANSIICode(writer: *Io.Writer, comptime code: ANSII_Code) !void {
+pub fn printANSICode(writer: *Io.Writer, comptime code: ANSI_Code) !void {
     try writer.print(codeToRaw(code), .{});
     try writer.flush();
 }
